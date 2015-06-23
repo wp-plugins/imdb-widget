83e5bafcc73ebc464d43c877c9a3fc88306eb490
hacdias <hacdias@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-23 09:28:48 +0000

    v1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/imdb-widget/tags/1.0.6@1186069 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ vendor/refactors/smart-scraper/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the SmartScraper package.
+ *
+ * (c) Henrique Dias <hacdias@gmail.com>
+ * (c) Luís Soares <lsoares@gmail.com>
+ *
+ * Licensed under the MIT license.
+ */
+
+require 'vendor/autoload.php';
